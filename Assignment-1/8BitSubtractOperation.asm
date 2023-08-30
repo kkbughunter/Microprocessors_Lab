b6362770450b48c4fb710bafa962c492d5f7d2dd
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-08-30 09:56:52 +0530

    Update 8BitSubtractOperation.asm

--- Assignment-1/8BitSubtractOperation.asm
+++ Assignment-1/8BitSubtractOperation.asm
@@ -1,10 +1,10 @@
-MOV AL, [2000]   ; Move the byte at memory address 2000 into AL
-MOV BL, [2001]   ; Move the byte at memory address 2001 into BL
-MOV CL, 00       ; Move the value 00 into CL (clear CL)
-SUB AL, BL       ; Subtract the value in BL from AL and store the result in AL
-JN LABLE         ; Jump to the label LABLE if the result is negative (JN means Jump if Negative)
-INC CL           ; Increment the value in CL by 1
-NEG AL           ; Negate (change the sign of) the value in AL
-MOV [2002], AL   ; Move the value in AL to memory address 2002
-MOV [2003], CL   ; Move the value in CL to memory address 2003
-HTL              ; Halt the program
+MOV AH, [2000]   ; AH <- [2000]
+MOV BH, [2001]   ; BH <- [2002]
+MOV CH, 00       ; CH <- 00
+ADD AH, BH       ; AH <- AH + BH
+JNC 100C         ; Jump to address 100C if there's no carry (JNC means Jump if No Carry)
+INC CL           ; CH <- CH + 01
+NEG AH  	 ; 2S complement of AH
+MOV [2002], AH   ; [2002] <- AH
+MOV [2003], CH   ; [2003] <- CH
+HLT              ; Halt the program
