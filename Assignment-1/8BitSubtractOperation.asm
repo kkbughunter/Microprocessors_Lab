055e51136169d3280715d00547bc5087628f8ad7
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-08-30 10:03:43 +0530

    Update 8BitSubtractOperation.asm

--- Assignment-1/8BitSubtractOperation.asm
+++ Assignment-1/8BitSubtractOperation.asm
@@ -1,7 +1,7 @@
 MOV AH, [2000]   ; AH <- [2000]
 MOV BH, [2001]   ; BH <- [2002]
 MOV CH, 00       ; CH <- 00
-ADD AH, BH       ; AH <- AH + BH
+SUB AH, BH       ; AH <- AH + BH
 JNC 100C         ; Jump to address 100C if there's no carry (JNC means Jump if No Carry)
 INC CL           ; CH <- CH + 01
 NEG AH  	 ; 2S complement of AH
