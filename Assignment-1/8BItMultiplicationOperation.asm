865f0d66bd58d29f79a1b698a11151b99c818aa9
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-08-30 09:49:16 +0530

    Update 8BItMultiplicationOperation.asm

--- Assignment-1/8BItMultiplicationOperation.asm
+++ Assignment-1/8BItMultiplicationOperation.asm
@@ -1,5 +1,6 @@
-MOV AL, [2000]   ; Move the byte at memory address 2000 into AL
-MOV BL, [2001]   ; Move the byte at memory address 2001 into BL
-MUL BL           ; Multiply AL by BL, and the result is stored in AX (the higher 16 bits) and DX (the lower 16 bits)
-MOV [2002], AL   ; Move the value in AL to memory address 2002
+MOV AL, [2000]   ; AL <- [2000]
+MOV BL, [2001]   ; BL <- [2001]
+MUL BL           ; AL <- AL * BL
+MOV [2002], AH   ; [2002] <- AL
+MOV [2003], AL   ; [2003] <- CL
 HTL              ; Halt the program
