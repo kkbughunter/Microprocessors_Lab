f8265648eb730ea292fff761ced7e096ca93876c
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-08-30 09:52:03 +0530

    Update 8BitDivitionOperation.asm

--- Assignment-1/8BitDivitionOperation.asm
+++ Assignment-1/8BitDivitionOperation.asm
@@ -1,6 +1,7 @@
-MOV AL, [2000]   ; Move the byte at memory address 2000 into AL
-MOV BL, [2001]   ; Move the byte at memory address 2001 into BL
-MOV CL, 00       ; Move the value 00 into CL (clear CL)
-DIV BL           ; Divide AL by BL, and the quotient is stored in AL, and the remainder in AH
-MOV [2002], AL   ; Move the quotient (in AL) to memory address 2002
+MOV AH, 00       ; AH <- 00
+MOV AL, [2000]   ; AL <- [2000]
+MOV BL, [2001]   ; BL <- [2001]
+DIV BL           ; AX <- AL / BL
+MOV [2002], AL   ; [2002] <- AL (quotient)
+MOV [2003], AH   ; [2003] <- AH (remainder)
 HTL              ; Halt the program
