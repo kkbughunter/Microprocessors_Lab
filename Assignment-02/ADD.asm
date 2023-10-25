c01cba2df8bcfa199fcf08fc564f7c8705f2db83
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-10-24 20:59:28 -0700

    Update ADD.asm

--- Assignment-02/ADD.asm
+++ Assignment-02/ADD.asm
@@ -3,8 +3,7 @@ MOV BX, [2002]   ; BX <- [2002]
 MOV CH, 00       ; CH <- 00
 ADD AX, BX       ; AX <- AX + BX
 JNC 100C         ; Jump to address 100C if there's no carry (JNC means Jump if No Carry)
-INC CH           ; CH <- CH + 01
-NEG AX  	 ; 2S complement of AX
+INC CH           ; CH <- CH + 01
 MOV [2004], AX   ; [2004] <- AX
 MOV [2006], CH   ; [2006] <- CH
 HLT              ; Halt the program
